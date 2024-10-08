2c3d25d5a88eb61360fab59d87a37c05d7e46cd9
merekekbtu <m_bazarbayev@kbtu.kz>
2024-10-08 21:41:37 +0500

    Update ex_39.cpp

--- ex_39.cpp
+++ ex_39.cpp
@@ -2,16 +2,16 @@
 #include <set>
 
 int main() {
-    std::set<int> seen_numbers;  // Множество для хранения встреченных чисел
+    std::set<int> seen_numbers;  
     int number;
 
-    // Чтение последовательности чисел
+    
     while (std::cin >> number) {
         if (seen_numbers.find(number) != seen_numbers.end()) {
             std::cout << "YES" << std::endl;
         } else {
             std::cout << "NO" << std::endl;
-            seen_numbers.insert(number);  // Добавляем число в множество
+            seen_numbers.insert(number);  
         }
     }
 
