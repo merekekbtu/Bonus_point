4f8963322da34bac3279338ce61a74ed4d47c49e
merekekbtu <m_bazarbayev@kbtu.kz>
2024-10-08 21:48:30 +0500

    Update ex_37.cpp

--- ex_37.cpp
+++ ex_37.cpp
@@ -6,21 +6,21 @@ int main() {
     set<int> set1, set2;
     int num;
 
-    // Ввод первого списка
+    
     while (cin.peek() != '\n') {
         cin >> num;
         set1.insert(num);
     }
 
-    // Пропустить новую строку
+    
     cin.ignore();
 
-    // Ввод второго списка
+    
     while (cin >> num) {
         set2.insert(num);
     }
 
-    // Находим пересечение множеств
+    
     int count = 0;
     for (int x : set1) {
         if (set2.find(x) != set2.end()) {
@@ -28,7 +28,7 @@ int main() {
         }
     }
 
-    // Выводим результат
+    
     cout << count << endl;
 
     return 0;
